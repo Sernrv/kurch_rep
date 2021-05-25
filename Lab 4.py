1cdcb1c441dac142a2e9d8620612dcff77bec2c5
sernrv <sernrv@yandex.ru>
2021-05-25 03:38:46 +0300

    wrote a new program

--- Lab 4.py
+++ Lab 4.py
@@ -1,4 +1,16 @@
 import mpmath as mpm
 import plotly as pl
+from numba import njit
 
+
+@njit(fastmath=True, cache=True, parallel=True)
 def rung(a, b):
+    for i in range(b - a):
+        y = list(map(mpm.cos(), i))
+        return y
+
+
+x1 = int(input())
+x2 = int(input())
+b = rung(x1, x2)
+print(b)
