e404625817170eb1e2f2cf5ea1edae13c7045ec6
sernrv <sernrv@yandex.ru>
2022-04-06 05:38:17 +0300

    Raise added

--- Exception_testing.py
+++ Exception_testing.py
@@ -1,8 +1,19 @@
-#%%
+# %% Finally construction
 a = float(input())
 try:
     b = 7 / a
-except:
+except ZeroDivisionError:
     print('Все неправильно!')
 finally:
     print('В любом случае неправильно...')
+
+
+# %% Raise construction
+class TestException(Exception):
+    pass
+
+
+a = float(input())
+b = 5 / a
+if a == 1:
+    raise TestException('One division exception')
